8454d7eb90859deef1a5ba96d4acf45ca73b077e
Jay Borad <87151841+JBORAD988@users.noreply.github.com>
2024-01-25 18:12:44 +0530

    Delete src/app/constants/config.ts

--- src/app/constants/config.ts
+++
@@ -1,5 +0,0 @@
-export const tmdbcConfig = {
- apikey:"919d20d22efa5ce128102d923cf27268",
-  "accessToken":"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MTlkMjBkMjJlZmE1Y2UxMjgxMDJkOTIzY2YyNzI2OCIsInN1YiI6IjY1YjIzMWM4YTgwMjM2MDE4NmFlZGFlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sEtHLYYJYzCUSCtCrP_F5wQf1D3gRpD218Whj4z_Vjc"
-
-}
